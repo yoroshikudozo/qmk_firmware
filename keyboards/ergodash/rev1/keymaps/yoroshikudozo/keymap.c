b6c89867ee620b76a15ed991fb0ceb3084024e2d
kudo <yosuke.kudo910@gmail.com>
2020-07-31 23:16:30 +0900

    Add grave

--- keyboards/ergodash/rev1/keymaps/yoroshikudozo/keymap.c
+++ keyboards/ergodash/rev1/keymaps/yoroshikudozo/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Ctrl |   A  |   S  |   D  |   F  |   G  |   \  |                    |   "  |   H  |   J  |   K  |   L  |   ;  | Enter|
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  | Caps |                    | Enter|   N  |   M  |   ,  |   .  |  Up  | Shift|
+   * | Shift|   Z  |   X  |   C  |   V  |   B  | Caps |                    |   `  |   N  |   M  |   ,  |   .  |  Up  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
    * | ALt  |  GUI | Ctrl |||||||| EISU | Lower| Space|  Del |||||||| Bksp | Enter| Raise| KANA |||||||| Left | Down | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,                        KC_EQL , KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                        KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_SLSH, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_PIPE,                        KC_DQT , KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,  \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_CAPS,                        KC_ENT , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_RSFT, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_CAPS,                        KC_GRV , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_RSFT, \
     KC_LALT, KC_LGUI, KC_LCTL,          KC_LANG2,LOWER,   KC_SPC ,KC_DEL,         KC_BSPC,KC_ENT , RAISE,   KC_LANG1,         KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
 
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Tab  |   1  |   2  |   3  |   4  |   5  |   |  |                    |   "  |   *  | Left | Down | Right|   :  | Enter|
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | Shift|   6  |   7  |   8  |   9  |   0  | Space|                    | Enter|   N  |   M  |   <  |   >  |   ?  | Shift|
+   * | Shift|   6  |   7  |   8  |   9  |   0  | Space|                    | Enter|   N  |   `  |   <  |   >  |   ?  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
    * | Ctrl |  GUI |  ALt | EISU |||||||| Lower| Space|  Del |||||||| Bksp | Enter| Raise|||||||| Home |PageDn|PageUp|  End |
    * ,----------------------------------------------------------------------------------------------------------------------.
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_UNDS,                        KC_PLUS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_LCBR,                        KC_RCBR, KC_CIRC, KC_AMPR, KC_UP,   KC_LPRN, KC_RPRN, KC_PIPE, \
     KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_PIPE,                        KC_QUOT, KC_AMPR, KC_LEFT, KC_DOWN, KC_RGHT, KC_COLN, KC_ENT , \
-    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_CAPS,                        KC_ENT , KC_N,    KC_M,    KC_LT,   KC_GT,   KC_QUES, KC_RSFT, \
+    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_CAPS,                        KC_ENT , KC_N,    KC_GRV,  KC_LT,   KC_GT,   KC_QUES, KC_RSFT, \
     KC_LALT, KC_LGUI, KC_LCTL,          KC_LANG2,LOWER,   KC_SPC ,KC_DEL,         KC_BSPC,KC_ENT , RAISE,   KC_LANG1,         KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
 
